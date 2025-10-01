fb5f0932cfc4cbcb51034aa439ebc8deca2aa52b
João Yamamoto Angelo <141883422+JaoYamamoto@users.noreply.github.com>
2025-10-01 08:38:14 -0300

    MVC
    começando a adaptar o projeto ao modelo MVC

---
+++ models/models.py
@@ -0,0 +1,23 @@
+from app import db
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    author = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer)
+    genre = db.Column(db.String(255))
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'year': self.year,
+            'genre': self.genre,
+            'description': self.description
+        }
